b942a4d4744545defb300199a44bc4456f566fa0
Akhiladeva <akhiladevac@gmail.com>
2021-08-25 16:11:45 +0530

    kj

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'utils/theme.dart';
-// void main() {
-//   runApp(MyApp());
-// }
-//
+
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
 //   @override
